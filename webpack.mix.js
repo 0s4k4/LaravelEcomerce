967700b2243bded5dfa70ff6a8061efe091bb7ed
0s4k4 <soulbosss@gmail.com>
2021-04-04 23:26:21 -0500

    se añadio la pagina de panel de administracion, se creo url de admin y se añadio webpack de laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ //directorio de recursos 
+
+ mix.copyDirectory('resources/backend','public/backend');
+ mix.copyDirectory('resources/frontend','public/frontend');
